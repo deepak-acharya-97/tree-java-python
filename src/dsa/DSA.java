f67142490be6129836df73d6a27fcc03fedd130d
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-02-24 18:28:21 +0530

    Update Folder Structure

--- src/dsa/DSA.java
+++ src/dsa/DSA.java
@@ -1,29 +1,21 @@
 package dsa;
+import util.Common.Utility;
+import dsa.SampleTree.TreeDefinition;
 
 public class DSA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Node root=new Node(50, new Node(30), new Node(70));
-		root.getLeft().setLeftRight(new Node(20), new Node(40));
-		root.getRight().setLeftRight(new Node(60), new Node(80));	
+		Node root = TreeDefinition.getSampleTree();	
 		printBst(root);
 	}
 	
-	public static <T> void Print(T message) {
-		System.out.println(message);
-	}
-	
-	public static <T> boolean isNotNull(T obj) {
-		return (obj != null);
-	}
-
 	private static void printBst(Node root) {
 		// TODO Auto-generated method stub
-		if(!isNotNull(root)) {
+		if(!Utility.isNotNull(root)) {
 			return;
 		}
-		Print(root.getData());
+		Utility.Print(root.getData());
 		printBst(root.getLeft());
 		printBst(root.getRight());
 	}
